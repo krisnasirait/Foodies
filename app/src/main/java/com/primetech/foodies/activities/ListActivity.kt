f45edba054769f6b61e941afa2c7a285c7e98747
Krisna Sirait <krisnapande00@outlook.com>
2022-12-01 10:58:20 +0700

    create activity needed

--- app/src/main/java/com/primetech/foodies/activities/ListActivity.kt
+++ app/src/main/java/com/primetech/foodies/activities/ListActivity.kt
@@ -1,11 +1,12 @@
-package com.primetech.foodies
+package com.primetech.foodies.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.primetech.foodies.R
 
-class MainActivity : AppCompatActivity() {
+class ListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_list)
     }
 }
---
+++ app/src/main/java/com/primetech/foodies/activities/MapActivity.kt
@@ -0,0 +1,12 @@
+package com.primetech.foodies.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.primetech.foodies.R
+
+class MapActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+    }
+}
